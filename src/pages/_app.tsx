96468f728bb8303bea2f4a20859b4bbe18c0f8bf
Ryan <racollette@gmail.com>
2021-07-28 21:12:13 -0600

    fix: app metadata

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -94,18 +94,18 @@ function MyApp({
           name="viewport"
           content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
         />
-        <title key="title">SUSHI</title>
+        <title key="title">Elavate Finance</title>
 
         <meta
           key="description"
           name="description"
-          content="Be a DeFi Chef with Sushi. Swap, earn, stack yields, lend, borrow, leverage all on one decentralized, community driven platform. Welcome home to DeFi"
+          content="Home for DeFi on Elastos. Swap, earn, stack yields all on one decentralized, community driven platform."
         />
 
-        <meta name="application-name" content="SUSHI App" />
+        <meta name="application-name" content="Elavate Finance" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-        <meta name="apple-mobile-web-app-title" content="SUSHI App" />
+        <meta name="apple-mobile-web-app-title" content="Elavate Finance" />
 
         <meta name="format-detection" content="telephone=no" />
         <meta name="mobile-web-app-capable" content="yes" />
@@ -114,16 +114,16 @@ function MyApp({
         <meta name="theme-color" content="#F338C3" />
 
         <meta key="twitter:card" name="twitter:card" content="app" />
-        <meta key="twitter:image" name="twitter:image" content="https://app.sushi.com/icons/icon-192x192.png" />
-        <meta key="twitter:creator" name="twitter:creator" content="@SushiSwap" />
+        <meta key="twitter:image" name="twitter:image" content="android-chrome-192x192.png" />
+        <meta key="twitter:creator" name="twitter:creator" content="@ElavateFinance" />
         <meta key="og:type" property="og:type" content="website" />
-        <meta key="og:site_name" property="og:site_name" content="SUSHI App" />
-        <meta key="og:url" property="og:url" content="https://app.sushi.com" />
-        <meta key="og:image" property="og:image" content="https://app.sushi.com/apple-touch-icon.png" />
+        <meta key="og:site_name" property="og:site_name" content="Elavate Finance" />
+        <meta key="og:url" property="og:url" content="https://elavate.finance" />
+        <meta key="og:image" property="og:image" content="/apple-touch-icon.png" />
         <meta
           key="og:description"
           property="og:description"
-          content="Be a DeFi Chef with Sushi. Swap, earn, stack yields, lend, borrow, leverage all on one decentralized, community driven platform. Welcome home to DeFi"
+          content="Home for DeFi on Elastos. Swap, earn, stack yields all on one decentralized, community driven platform."
         />
 
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
